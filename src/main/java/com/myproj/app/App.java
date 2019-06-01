8b3b9a8ef63f32b6ac38702e0e4a516e98046f21
root <root@ip-172-31-7-5.us-east-2.compute.internal>
2019-06-01 03:38:03 +0000

    1st commit of myapp1 version1.0

---
+++ src/main/java/com/myproj/app/App.java
@@ -0,0 +1,13 @@
+package com.myproj.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this jar is created manually for further learning in 2nd class of jenkins!" );
+    }
+}
